1d49aa5dfa11d0f012b4d2b438889256bdfa3d51
Priya Tiwari <priya.tiwari@comviva.com>
2026-09-23 10:09:32 +0530

    Initial Project Commit

---
+++ src/test/java/com/example/loggingsecurity/LoggingsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggingsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
